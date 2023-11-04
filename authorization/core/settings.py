b0ca91cfc347c42d8f33b1bc69a1a75aba543f81
arad <aradarvinxo@gmail.com>
2023-11-04 17:33:00 +0330

    Chore: app settings

---
+++ authorization/core/settings.py
@@ -0,0 +1,25 @@
+from decouple import config
+
+
+JWT_SECRET = config("JWT_SECRET_KEY")
+JWT_ALGORITHM = config("JWT_ALGORITHM")
+
+TIME_DICT = {
+    "A":"seconds",
+    "B":"minutes",
+    "C":"hours",
+    "D":"days"
+}
+
+ACCESS_TOKEN_TIME_UNIT = TIME_DICT["B"]
+REFRESH_TOKEN_TIME_UNIT = TIME_DICT["B"]
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 5
+REFRESH_TOKEN_EXPIRE_MINUTES = 10
+
+
+REDIS_HOST_ADDRESS = "redis://127.0.0.1:6379/0"
+MONGODB_HOST_ADDRESS = "mongodb://localhost:27017/"
+
+ACCOUNT_ADDRESS = "http://127.0.0.1:8000/account"
+DATA_ADDRESS = "http://127.0.0.1:8000/data"
