0f076b7d81f7ebb9bc2e3b074935165f57ef10d1
arad <aradarvinxo@gmail.com>
2023-11-04 17:32:46 +0330

    Chore: httpx method to account update data

--- authorization/connection/httpx_manager.py
+++ authorization/connection/httpx_manager.py
@@ -21,3 +21,9 @@ async def httpx_response_mongodb_data(data_endpoint: str, data: dict = None):
 
 
 
+async def httpx_response_mongodb_data_update(data_endpoint: str, data: dict = None):
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(f"{settings.DATA_ADDRESS}/{data_endpoint}", json=data)
+
+    return response.json()
