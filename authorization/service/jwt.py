84bbcb0f677a6ab54b2910adfa133e2d2b901229
arad <aradarvinxo@gmail.com>
2023-11-04 17:44:42 +0330

    Chore: init method

--- authorization/service/jwt.py
+++ authorization/service/jwt.py
@@ -7,3 +7,11 @@ from connection.httpx_manager import httpx_response_mongodb_data
 
 class JWTService:
 
+    HOST_ADDRESS: str = settings.REDIS_HOST_ADDRESS 
+
+
+    def __init__(self) -> None:
+        self.redis = aioredis.from_url(self.HOST_ADDRESS, decode_responses=True)
+
+
+
