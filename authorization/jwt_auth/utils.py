6cdcf98752fa2f11ada51c62dc1629e6d3738e23
arad <aradarvinxo@gmail.com>
2023-11-04 17:42:21 +0330

    Chore: create access token from refresh method

--- authorization/jwt_auth/utils.py
+++ authorization/jwt_auth/utils.py
@@ -22,5 +22,19 @@ async def create_access_token(user_id: str) -> str:
                          jti=jti)
 
 
+async def create_access_token_from_refresh(user_id: str, ref_token_payload: dict) -> str: 
+    expire = await token_exp_time(settings.ACCESS_TOKEN_TIME_UNIT, settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    if expire > datetime.utcfromtimestamp(int(ref_token_payload["exp"])):
+        expire = datetime.utcfromtimestamp(int(ref_token_payload["exp"]))
+
+    token_jti = ref_token_payload["jti"]
+
+    return await _create_token(token_type="access", 
+                         user_id=user_id, 
+                         jti=token_jti,
+                         expire=expire)
+
+
 
 
